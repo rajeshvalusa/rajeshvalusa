0f6b199a836994fae9f6502efd56d1b4f96379fb
rajeshvalusa <rajeshkumarvalusa366@gmail.com>
2023-06-07 18:17:01 +0530

    changes

---
+++ src/Sample/SampleCode.java
@@ -0,0 +1,19 @@
+package Sample;
+
+public class SampleCode {
+
+	public static void main(String[] args) {
+		        int a=0;
+				int b=1;
+				while(a>30) {
+					int c=a+b;
+					System.out.println(c);
+					a=b;
+					b=c;
+				}
+
+			}
+
+		}
+
+	
